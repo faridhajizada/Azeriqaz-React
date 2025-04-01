624911f80d920dbf3e74457a72c76a0a39c0ae64
Hajizada Farid <faridmh@code.edu.az>
2025-04-01 11:59:23 +0400

    first commit

---
+++ src/app/modules/auth/components/Login.tsx
@@ -0,0 +1,159 @@
+import { useState } from "react";
+import * as Yup from "yup";
+import clsx from "clsx";
+import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import { getUserByToken, login } from "../core/_requests";
+import { toAbsoluteUrl } from "../../../../_metronic/helpers";
+import { useAuth } from "../core/Auth";
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Yanlış email formatı")
+    .min(3, "Minimum 3 simvol")
+    .max(50, "Maksimum 50 simvol")
+    .required("Email tələb olunur"),
+  password: Yup.string()
+    .min(3, "Minimum 3 simvol")
+    .max(50, "Maksimum 50 simvol")
+    .required("Şifrə tələb olunur"),
+});
+
+const initialValues = {
+  email: "admin@demo.com",
+  password: "demo",
+};
+
+export function Login() {
+  const [loading, setLoading] = useState(false);
+  const { saveAuth, setCurrentUser } = useAuth();
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: loginSchema,
+    onSubmit: async (values, { setStatus, setSubmitting }) => {
+      setLoading(true);
+      try {
+        const { data: auth } = await login(values.email, values.password);
+        saveAuth(auth);
+        const { data: user } = await getUserByToken(auth.api_token);
+        setCurrentUser(user);
+      } catch (error) {
+        console.error(error);
+        saveAuth(undefined);
+        setStatus("Giriş məlumatları səhvdir");
+        setSubmitting(false);
+        setLoading(false);
+      }
+    },
+  });
+
+  return (
+    <form
+      className="form w-100"
+      onSubmit={formik.handleSubmit}
+      noValidate
+      id="kt_login_signin_form"
+    >
+      {/* begin::Heading */}
+      <div className="text-center mb-11">
+        <h1 className="text-gray-900 fw-bolder mb-3">Giriş</h1>
+      </div>
+      {/* begin::Heading */}
+
+      {formik.status ? (
+        <div className="mb-lg-15 alert alert-danger">
+          <div className="alert-text font-weight-bold">{formik.status}</div>
+        </div>
+      ) : (
+        <></>
+      )}
+
+      {/* begin::Form group */}
+      <div className="fv-row mb-8">
+        <label className="form-label fs-6 fw-bolder text-gray-900">Email</label>
+        <input
+          placeholder="Email və ya Tabel nömrəsi "
+          {...formik.getFieldProps("email")}
+          className={clsx(
+            "form-control bg-transparent",
+            { "is-invalid": formik.touched.email && formik.errors.email },
+            {
+              "is-valid": formik.touched.email && !formik.errors.email,
+            }
+          )}
+          type="email"
+          name="email"
+          autoComplete="off"
+        />
+        {formik.touched.email && formik.errors.email && (
+          <div className="fv-plugins-message-container">
+            <span role="alert">{formik.errors.email}</span>
+          </div>
+        )}
+      </div>
+      {/* end::Form group */}
+
+      {/* begin::Form group */}
+      <div className="fv-row mb-3">
+        <label className="form-label fw-bolder text-gray-900 fs-6 mb-0">
+          Şifrə
+        </label>
+        <input
+          type="password"
+          placeholder="Şifrə"
+          autoComplete="off"
+          {...formik.getFieldProps("password")}
+          className={clsx(
+            "form-control bg-transparent",
+            {
+              "is-invalid": formik.touched.password && formik.errors.password,
+            },
+            {
+              "is-valid": formik.touched.password && !formik.errors.password,
+            }
+          )}
+        />
+        {formik.touched.password && formik.errors.password && (
+          <div className="fv-plugins-message-container">
+            <div className="fv-help-block">
+              <span role="alert">{formik.errors.password}</span>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* end::Form group */}
+
+      {/* begin::Wrapper */}
+      <div className="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+        <div />
+
+        {/* begin::Link */}
+        {/* <Link to="/auth/forgot-password" className="link-primary">
+          Forgot Password ?
+        </Link> */}
+        {/* end::Link */}
+      </div>
+      {/* end::Wrapper */}
+
+      {/* begin::Action */}
+      <div className="d-grid mb-10">
+        <button
+          type="submit"
+          id="kt_sign_in_submit"
+          className="btn btn-primary"
+          disabled={formik.isSubmitting || !formik.isValid}
+        >
+          {!loading && <span className="indicator-label">Daxil ol</span>}
+          {loading && (
+            <span className="indicator-progress" style={{ display: "block" }}>
+              Please wait...
+              <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
+            </span>
+          )}
+        </button>
+      </div>
+      {/* end::Action */}
+    </form>
+  );
+}
