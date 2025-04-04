eed31f7fcd69116fece8d78b72af3f28e2d5b6a5
Hajizada Farid <faridmh@code.edu.az>
2025-04-04 13:50:10 +0400

    feat: add ListFooter

--- src/app/pages/accident-calls/Call/Call.tsx
+++ src/app/pages/accident-calls/Call/Call.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Content } from "./../../../../_metronic/layout/components/content/Content";
 import CallListHeader from "./../Call/CallListHeader/CallListHeader";
 import CallList from "./CallList/CallList";
+import CallListFooter from "./CallListFooter/CallListFooter";
 
 function Call() {
   return (
@@ -9,6 +10,7 @@ function Call() {
       <div className={`card mb-5 mb-xl-8`}>
         <CallListHeader />
         <CallList />
+        <CallListFooter />
       </div>
     </Content>
   );
