2a19dd2455f39ba1607ef5d5078fadd0e7c5bdf4
Hajizada Farid <faridmh@code.edu.az>
2025-04-10 09:40:04 +0400

    feat: refactor Show component header in accident-calls

--- src/app/pages/accident-calls/Call/List/Show/Show.tsx
+++ src/app/pages/accident-calls/Call/List/Show/Show.tsx
@@ -21,27 +21,8 @@ function Show({ show, handleClose }: ShowProps) {
       dialogClassName="modal-dialog-metronic"
       contentClassName="modal-content-metronic"
     >
-<Modal.Header closeButton className="bg-primary">
-          <Modal.Title className="text-white">
-          <span className="svg-icon svg-icon-2x">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <path
-                opacity="0.3"
-                d="M21 22H3C2.4 22 2 21.6 2 21V5C2 4.4 2.4 4 3 4H21C21.6 4 22 4.4 22 5V21C22 21.6 21.6 22 21 22Z"
-                fill="currentColor"
-              />
-              <path
-                d="M6 6C5.4 6 5 5.6 5 5V3C5 2.4 5.4 2 6 2C6.6 2 7 2.4 7 3V5C7 5.6 6.6 6 6 6ZM11 5V3C11 2.4 10.6 2 10 2C9.4 2 9 2.4 9 3V5C9 5.6 9.4 6 10 6C10.6 6 11 5.6 11 5ZM15 5V3C15 2.4 14.6 2 14 2C13.4 2 13 2.4 13 3V5C13 5.6 13.4 6 14 6C14.6 6 15 5.6 15 5ZM19 5V3C19 2.4 18.6 2 18 2C17.4 2 17 2.4 17 3V5C17 5.6 17.4 6 18 6C18.6 6 19 5.6 19 5Z"
-                fill="currentColor"
-              />
-            </svg>
-          </span>
+      <Modal.Header closeButton className="bg-primary">
+        <Modal.Title className="text-white">
           <span className="ms-3">Qəza çağırışı #AQ-2024-1</span>
         </Modal.Title>
       </Modal.Header>
