e62cd2b2fa3a7f16be54d68c919c6810ebab075f
Hajizada Farid <faridmh@code.edu.az>
2025-04-11 11:32:16 +0400

    fear: optimization route*

--- src/app/routing/PrivateRoutes.tsx
+++ src/app/routing/PrivateRoutes.tsx
@@ -1,298 +1,180 @@
-import { lazy, FC, Suspense } from "react";
+import React from "react";
+import { lazy, FC, Suspense, useMemo } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { MasterLayout } from "../../_metronic/layout/MasterLayout";
 import TopBarProgress from "react-topbar-progress-indicator";
 import { DashboardWrapper } from "../pages/dashboard/DashboardWrapper";
-// import { MenuTestPage } from "../pages/MenuTestPage";
 import { getCSSVariableValue } from "../../_metronic/assets/ts/_utils";
 import { WithChildren } from "../../_metronic/helpers";
 import BuilderPageWrapper from "../pages/layout-builder/BuilderPageWrapper";
 
+const components = {
+  Users: {
+    component: lazy(() => import("../pages/users/Users")),
+    path: "users",
+  },
+
+  AccidentCalls: {
+    Call: {
+      component: lazy(() => import("../pages/accident-calls/Call/Call")),
+      path: "call",
+    },
+    Car: {
+      component: lazy(() => import("../pages/accident-calls/Car/Car")),
+      path: "car",
+    },
+    Category: {
+      component: lazy(
+        () => import("../pages/accident-calls/Category/Category")
+      ),
+      path: "category",
+    },
+    Statistics: {
+      component: lazy(
+        () => import("../pages/accident-calls/Statistics/Statistics")
+      ),
+      path: "statistics",
+    },
+  },
+
+  Settings: {
+    Role: {
+      component: lazy(() => import("../pages/settings/Role/Role")),
+      path: "role",
+    },
+    Permission: {
+      component: lazy(() => import("../pages/settings/Permission/Permission")),
+      path: "permission",
+    },
+    Module: {
+      component: lazy(() => import("../pages/settings/Module/Module")),
+      path: "module",
+    },
+    ProcessLog: {
+      component: lazy(() => import("../pages/settings/ProcessLog/ProcessLog")),
+      path: "process-log",
+    },
+    Logs: {
+      component: lazy(() => import("../pages/settings/Logs/Logs")),
+      path: "logs",
+    },
+  },
+
+  TechExam: {
+    ExaminationPlan: {
+      component: lazy(
+        () => import("../pages/tech-exam/examination-plan/ExaminationPlan")
+      ),
+      path: "examination-plan",
+    },
+    ShutdownPlan: {
+      component: lazy(
+        () => import("../pages/tech-exam/shutdown-plan/ShutdownPan")
+      ),
+      path: "shutdown-plan",
+    },
+    Device: {
+      component: lazy(() => import("../pages/tech-exam/device/Device")),
+      path: "device",
+    },
+    Violation: {
+      component: lazy(() => import("../pages/tech-exam/violation/Violation")),
+      path: "violation",
+    },
+    ExaminationYearly: {
+      component: lazy(
+        () => import("../pages/tech-exam/examination-yearly/ExaminationYearly")
+      ),
+      path: "examination-yearly",
+    },
+    Contractor: {
+      component: lazy(() => import("../pages/tech-exam/contractor/Contractor")),
+      path: "contractor",
+    },
+    Examination: {
+      component: lazy(
+        () => import("../pages/tech-exam/examination/Examination")
+      ),
+      path: "examination",
+    },
+    TechStatistics: {
+      component: lazy(() => import("../pages/tech-exam/statistics/Statistics")),
+      path: "statistics",
+    },
+  },
+};
+
+const SuspensedView: FC<WithChildren> = ({ children }) => {
+  const baseColor = useMemo(() => getCSSVariableValue("--bs-primary"), []);
+
+  TopBarProgress.config({
+    barColors: { "0": baseColor },
+    barThickness: 1,
+    shadowBlur: 5,
+  });
+
+  return <Suspense fallback={<TopBarProgress />}>{children}</Suspense>;
+};
+
 const PrivateRoutes = () => {
-  // const ProfilePage = lazy(() => import("../modules/profile/ProfilePage"));
-  // const WizardsPage = lazy(() => import("../modules/wizards/WizardsPage"));
-  // const AccountPage = lazy(() => import("../modules/accounts/AccountPage"));
-  // const WidgetsPage = lazy(() => import("../modules/widgets/WidgetsPage"));
-  // const ChatPage = lazy(() => import("../modules/apps/chat/ChatPage"));
-  // const UsersPage = lazy(
-  //   () => import("../modules/apps/user-management/UsersPage")
-  // );
-
-  const Users = lazy(() => import("../pages/users/Users"));
-  const Call = lazy(() => import("../pages/accident-calls/Call/Call"));
-  const Car = lazy(() => import("./../pages/accident-calls/Car/Car"));
-  const Category = lazy(
-    () => import("./../pages/accident-calls/Category/Category")
-  );
-  const Statistics = lazy(
-    () => import("./../pages/accident-calls/Statistics/Statistics")
-  );
-
-  const Role = lazy(() => import("../pages/settings/Role/Role"));
-  const Permission = lazy(
-    () => import("../pages/settings/Permission/Permission")
-  );
-  const Module = lazy(() => import("../pages/settings/Module/Module"));
-  const Logs = lazy(() => import("../pages/settings/Logs/Logs"));
-  const ProcessLog = lazy(
-    () => import("../pages/settings/ProcessLog/ProcessLog")
-  );
-
-  const ExaminationPlan = lazy(
-    () => import("../pages/tech-exam/examination-plan/ExaminationPlan")
-  );
-  const ShutdownPan = lazy(
-    () => import("../pages/tech-exam/shutdown-plan/ShutdownPan")
-  );
-
-  const Device = lazy(() => import("../pages/tech-exam/device/Device"));
-  const Violation = lazy(
-    () => import("../pages/tech-exam/violation/Violation")
-  );
-  const ExaminationYearly = lazy(
-    () => import("../pages/tech-exam/examination-yearly/ExaminationYearly")
-  );
-
-  const Contractor = lazy(
-    () => import("../pages/tech-exam/contractor/Contractor")
-  );
-
-  const Examination = lazy(
-    () => import("../pages/tech-exam/examination/Examination")
-  );
-
-  const TechStatistics = lazy(
-    () => import("../pages/tech-exam/statistics/Statistics")
-  );
-
   return (
     <Routes>
       <Route element={<MasterLayout />}>
-        {/* Redirect to Dashboard after success login/registartion */}
         <Route path="auth/*" element={<Navigate to="/dashboard" />} />
+
         {/* Pages */}
         <Route path="dashboard" element={<DashboardWrapper />} />
         <Route path="builder" element={<BuilderPageWrapper />} />
+
         <Route
           path="/dashboard/users"
           element={
             <SuspensedView>
-              <Users />
-            </SuspensedView>
-          }
-        />
-        {/* Accident-calls */}
-        <Route
-          path="/dashboard/accident-calls/call"
-          element={
-            <SuspensedView>
-              <Call />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/accident-calls/car"
-          element={
-            <SuspensedView>
-              <Car />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/accident-calls/category"
-          element={
-            <SuspensedView>
-              <Category />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/accident-calls/statistics"
-          element={
-            <SuspensedView>
-              <Statistics />
-            </SuspensedView>
-          }
-        />
-        {/* Accident-calls */}
-
-        {/* Settings */}
-        <Route
-          path="/dashboard/settings/role"
-          element={
-            <SuspensedView>
-              <Role />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/settings/permission"
-          element={
-            <SuspensedView>
-              <Permission />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/settings/module"
-          element={
-            <SuspensedView>
-              <Module />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/settings/logs"
-          element={
-            <SuspensedView>
-              <Logs />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/settings/process-log"
-          element={
-            <SuspensedView>
-              <ProcessLog />
-            </SuspensedView>
-          }
-        />
-        {/* Settings */}
-
-        {/* Texniki müayinə */}
-        <Route
-          path="/dashboard/tech-exam/examination-plan"
-          element={
-            <SuspensedView>
-              <ExaminationPlan />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/tech-exam/shutdown-plan"
-          element={
-            <SuspensedView>
-              <ShutdownPan />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/tech-exam/device"
-          element={
-            <SuspensedView>
-              <Device />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/tech-exam/violation"
-          element={
-            <SuspensedView>
-              <Violation />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/tech-exam/examination-yearly"
-          element={
-            <SuspensedView>
-              <ExaminationYearly />
+              <components.Users.component />
             </SuspensedView>
           }
         />
 
-        <Route
-          path="/dashboard/tech-exam/contractor"
-          element={
-            <SuspensedView>
-              <Contractor />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/tech-exam/examination"
-          element={
-            <SuspensedView>
-              <Examination />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="/dashboard/tech-exam/statistics"
-          element={
-            <SuspensedView>
-              <TechStatistics />
-            </SuspensedView>
-          }
-        />
-        {/* Texniki müayinə */}
+        {Object.entries(components.AccidentCalls).map(
+          ([name, { component, path }]) => (
+            <Route
+              key={`accident-calls-${path}`}
+              path={`/dashboard/accident-calls/${path}`}
+              element={
+                <SuspensedView>{React.createElement(component)}</SuspensedView>
+              }
+            />
+          )
+        )}
 
-        {/* <Route path="menu-test" element={<MenuTestPage />} />
-        <Route
-          path="crafted/pages/profile/*"
-          element={
-            <SuspensedView>
-              <ProfilePage />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="crafted/pages/wizards/*"
-          element={
-            <SuspensedView>
-              <WizardsPage />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="crafted/widgets/*"
-          element={
-            <SuspensedView>
-              <WidgetsPage />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="crafted/account/*"
-          element={
-            <SuspensedView>
-              <AccountPage />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="apps/chat/*"
-          element={
-            <SuspensedView>
-              <ChatPage />
-            </SuspensedView>
-          }
-        />
-        <Route
-          path="apps/user-management/*"
-          element={
-            <SuspensedView>
-              <UsersPage />
-            </SuspensedView>
-          }
-        /> */}
-        {/* Page Not Found */}
+        {Object.entries(components.Settings).map(
+          ([name, { component, path }]) => (
+            <Route
+              key={`settings-${path}`}
+              path={`/dashboard/settings/${path}`}
+              element={
+                <SuspensedView>{React.createElement(component)}</SuspensedView>
+              }
+            />
+          )
+        )}
+
+        {Object.entries(components.TechExam).map(
+          ([name, { component, path }]) => (
+            <Route
+              key={`tech-exam-${path}`}
+              path={`/dashboard/tech-exam/${path}`}
+              element={
+                <SuspensedView>{React.createElement(component)}</SuspensedView>
+              }
+            />
+          )
+        )}
+
         <Route path="*" element={<Navigate to="/error/404" />} />
       </Route>
     </Routes>
   );
-};
-
-const SuspensedView: FC<WithChildren> = ({ children }) => {
-  const baseColor = getCSSVariableValue("--bs-primary");
-  TopBarProgress.config({
-    barColors: {
-      "0": baseColor,
-    },
-    barThickness: 1,
-    shadowBlur: 5,
-  });
-  return <Suspense fallback={<TopBarProgress />}>{children}</Suspense>;
 };
 
 export { PrivateRoutes };
