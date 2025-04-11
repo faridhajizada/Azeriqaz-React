e57423cf37a8ccac10c49e76b0fe85735f45abf1
Hajizada Farid <faridmh@code.edu.az>
2025-04-11 16:38:18 +0400

    feat: update device page

--- src/app/pages/tech-exam/device/Header/Create/Create.tsx
+++ src/app/pages/tech-exam/device/Header/Create/Create.tsx
@@ -29,43 +29,41 @@ const CreateCarModal: React.FC = () => {
   return (
     <>
       <Button variant="primary" onClick={openModal}>
-        <i className="ki-duotone ki-plus fs-2"></i> Yeni Səlahiyyət yarat
+        <i className="ki-duotone ki-plus fs-2"></i> Yeni Qurğu yarat
       </Button>
 
-      <Modal show={showModal} onHide={closeModal} centered size="md">
+      <Modal show={showModal} onHide={closeModal} centered size="sm">
         <Modal.Header closeButton className="bg-primary ">
-          <Modal.Title className="text-white">
-            Yeni Səlahiyyət yarat
-          </Modal.Title>
+          <Modal.Title className="text-white">Yeni Qurğu yarat</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
           <Form onSubmit={handleSubmit}>
             <div className="row">
-              <Form.Group className="col-md-6 mb-3">
-                <Form.Label>Səlahiyyət</Form.Label>
+              <Form.Group className="col-md-12 mb-3">
+                <Form.Label>Qurğu adı</Form.Label>
                 <Form.Control
                   type="text"
                   name="permission"
                   value={formData.permission}
                   onChange={handleChange}
-                  placeholder="Səlahiyyət"
+                  placeholder="Qurğu adı"
                 />
               </Form.Group>
 
-              <Form.Group className="col-md-6 mb-3">
-                <Form.Label>Səlahiyyət sistem adı</Form.Label>
+              <Form.Group className="col-md-12 mb-3">
+                <Form.Label>Kod</Form.Label>
                 <Form.Control
                   type="text"
                   name="permissionName"
                   value={formData.permissionName}
                   onChange={handleChange}
-                  placeholder="Səlahiyyət sistem adı"
+                  placeholder="Kod"
                 />
               </Form.Group>
 
-              <Form.Group className="col-md-6 mb-3">
-                <Form.Label>Modul</Form.Label>
+              <Form.Group className="col-md-12 mb-3">
+                <Form.Label>Kateqoriya</Form.Label>
                 <Form.Select
                   name="distmodulerict"
                   value={formData.module}
@@ -74,18 +72,6 @@ const CreateCarModal: React.FC = () => {
                   <option value="">---</option>
                   <option value="Nərimanov">Nərimanov</option>
                   <option value="Xətai">Xətai</option>
-                </Form.Select>
-              </Form.Group>
-
-              <Form.Group className="col-md-6 mb-3">
-                <Form.Label>İmkanlar</Form.Label>
-                <Form.Select
-                  name="ability"
-                  value={formData.ability}
-                  onChange={handleChange}
-                >
-                  <option value="">---</option>
-                  <option value="Elçin Məmmədov">Elçin Məmmədov</option>
                 </Form.Select>
               </Form.Group>
             </div>
